cf44d08d2e96a87ab54de033b95f2c1cb277113e
Darius Raisutis <dariusraisutis@gmail.com>
2023-01-14 14:07:24 +0100

    Login page handlers

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,12 +1,33 @@
+import axios from "axios";
+import { useState } from "react";
 import LoginComponent from "../components/LoginComponent";
 
-
 const Login = () => {
+    const [userName, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleOnNameChange = (event: React.SyntheticEvent<HTMLInputElement>) => {
+        const {preventDefault, currentTarget: { value }} = event;
+        preventDefault();
+        setUserName(value);
+    }
+
+    const handleOnPasswordChange = (event: React.SyntheticEvent<HTMLInputElement>) => {
+        const {preventDefault, currentTarget: { value }} = event;
+        preventDefault();
+        setPassword(value);
+    }
+
+    const handleSubmit = async (event: React.SyntheticEvent<HTMLFormElement>): Promise<void> => {
+        event.preventDefault();
+        let data = await axios.post('/login', {username: userName, password: password});
+    }
+
     return <>
         <LoginComponent 
-            handleSubmit={() => {}} 
-            onNameChange={() => {}} 
-            onPasswordChange={() => {}} 
+            handleSubmit={handleSubmit} 
+            onNameChange={handleOnNameChange} 
+            onPasswordChange={handleOnPasswordChange} 
         />        
     </>;
 }
